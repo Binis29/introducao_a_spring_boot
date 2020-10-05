966cf4ecdc3494d4380aae7eb0b71194a7acf4ae
binis29 <cleitonaguiarandrade@hotmail.com>
2020-10-05 20:28:03 -0300

    atualizado

--- servico-api-1-dia/src/main/java/com/servico/global/Controller.java
+++ servico-api-1-dia/src/main/java/com/servico/global/Controller.java
@@ -3,8 +3,11 @@ package com.servico.global;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,8 @@ public class Controller {
 	private ServicoRepository repository;
 
 	@GetMapping("/servicos")
-	public List<ServicoModel> pegarTodos() {
-
+	public List<ServicoModel> pegarTodos(@PathVariable String nome) {
 		return repository.findAll();
-
 	}
 	
 	//requerer a resposta save
@@ -28,5 +29,24 @@ public class Controller {
 		repository.save(model);
 		return model;	
 	}
+	
+	@GetMapping("/servicos/nome/{nome}")
+	public List<ServicoModel> buscarPorNome(@PathVariable String nome) {
+		return repository.findByNome(nome);
+	}
+
+	@PutMapping("/servicos/{id}")
+	public ServicoModel atualizar(@PathVariable Long id, @RequestBody ServicoModel model) {
+		model.setId(id);
+		repository.save(model);
+		return model;
+	}
+	
+	@DeleteMapping("/servicos/{id}")
+	public String remover(@PathVariable Long id) {	
+		repository.deleteById(id);
+	return "sucesso";
+	}
+	
 
 }
